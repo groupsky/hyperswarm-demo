bb396ef663375f8f1f51c0148295f64ca56bc317
Geno Roupsky <geno@roupsky.name>
2020-09-17 00:12:39 +0300

    Add better test logging

--- test.sh
+++ test.sh
@@ -9,11 +9,11 @@ $dc up -d
 
 echo "Checking the logs"
 pids=()
-timeout $timeout $dcl | grep -qxm1 "node1_1  |   + tcp incoming" &
+timeout $timeout $dcl | grep -Em1 "node1_1.*tcp incoming" || $(echo 'node1 failed' && false) &
 pids+=($!)
-timeout $timeout $dcl | grep -qxm1 "node2_1  |   + tcp incoming" &
+timeout $timeout $dcl | grep -Em1 "node2_1.*tcp incoming" || $(echo 'node2 failed' && false) &
 pids+=($!)
-timeout $timeout $dcl | grep -qEm2 "node3_1 +\| +\+ tcp .* local direct" &
+timeout $timeout $dcl | grep -Em2 "node3_1.*tcp .* local direct" || $(echo 'node3 failed' && false) &
 pids+=($!)
 wait "${pids[@]}"
 res=$?
